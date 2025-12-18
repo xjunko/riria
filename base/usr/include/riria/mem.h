0d72faca9baf8189ef0e14fa389f4d3fb1cf9111
xjunko <phony@kafu.ovh>
2025-12-18 15:55:16 +0800

    mem: improve the ux of page_directory_t
    also messing around with paging to see how it works cuz god damn this shit is hard (im dumb)

--- base/usr/include/riria/mem.h
+++ base/usr/include/riria/mem.h
@@ -6,13 +6,39 @@
 // pmm
 void kmalloc_start_at(uintptr_t);
 void* kmalloc(size_t);
+void* kmalloc_aligned(size_t, uintptr_t*);
 
 // vmm
 #define PG_PRESENT 0x1
 #define PG_RW 0x2
+#define PG_USER 0x4
+
+typedef struct page {
+  unsigned int present : 1;
+  unsigned int rw : 1;
+  unsigned int user : 1;
+  unsigned int writethrough : 1;
+  unsigned int cachedisable : 1;
+  unsigned int accessed : 1;
+  unsigned int dirty : 1;
+  unsigned int pat : 1;
+  unsigned int global : 1;
+  unsigned int unused : 3;
+  unsigned int frame : 20;
+} __attribute__((packed)) page_t;
+
+typedef struct page_table {
+  page_t pages[1024];
+} page_table_t;
+
+typedef struct page_directory {
+  uintptr_t physical_tables[1024];
+  page_table_t* tables[1024];
+  uintptr_t physical_address;
+} page_directory_t;
 
 void paging_initialize(void);
-void paging_load_directory(uint32_t);
+void paging_load_directory(page_directory_t*);
 void paging_finalize(void);
 
 void paging_fault(regs_t*);
--- kernel/mem/mem.c
+++ kernel/mem/mem.c
@@ -17,27 +17,71 @@ void* kmalloc(size_t sz) {
   return (void*)addr;
 }
 
+void* kmalloc_aligned(size_t sz, uintptr_t* phys_addr) {
+  if (bump_ptr &= 0xFFFFF000) {
+    bump_ptr = (bump_ptr & 0xFFFFF000) + 0x1000;
+  }
+
+  if (phys_addr) {
+    *phys_addr = bump_ptr;
+  }
+
+  uintptr_t addr = bump_ptr;
+  bump_ptr += sz;
+  return (void*)addr;
+}
+
 // vmm
-uint32_t page_dir[1024] __attribute__((aligned(4096)));
-uint32_t first_page_table[1024] __attribute__((aligned(4096)));  // first 4mb
+page_directory_t kernel_directory __attribute__((aligned(4096)));
+page_table_t first_table __attribute__((aligned(4096)));  // first 4mb
 
 void paging_initialize(void) {
   // identity map the first 4mb
+  page_table_t* table = &first_table;
   for (uint32_t i = 0; i < 1024; i++) {
-    first_page_table[i] = (i * 0x1000) | PG_PRESENT | PG_RW;
+    memset(&table->pages[i], 0, sizeof(page_t));
+    table->pages[i].present = 1;
+    table->pages[i].rw = 1;
+    table->pages[i].frame = i;
   }
 
-  // make sure page_dir is init to 0
-  // FIXME: can probably use memset for this?
+  // clear out everything first
   for (uint32_t i = 0; i < 1024; i++) {
-    page_dir[i] = 0;
+    kernel_directory.tables[i] = 0;
+    kernel_directory.physical_tables[i] = 0;
   }
 
-  page_dir[0] = ((uint32_t)first_page_table) | PG_PRESENT | PG_RW;
+  // set up the first 4mb table
+  kernel_directory.tables[0] = &first_table;
+  kernel_directory.physical_tables[0] =
+      (uint32_t)&first_table | PG_PRESENT | PG_RW;
+
+  // toy attempt
+  // this should make 0xDEADBEEF point to frame 0x0
+  uintptr_t phys_addr_test = 0xDEADBEEF;
+  phys_addr_test /= 0x1000;
+  uint32_t table_idx = phys_addr_test / 1024;
+  uint32_t page_idx = phys_addr_test % 1024;
+  printk("[mem] 0xDEADBEEF table_idx=%d page_idx=%d \n", table_idx, page_idx);
+
+  uint32_t test_table_phys_addr;
+  page_table_t* test_table = (page_table_t*)kmalloc_aligned(
+      sizeof(page_table_t), (uintptr_t*)&test_table_phys_addr);
+
+  test_table->pages[page_idx].present = 1;
+  test_table->pages[page_idx].rw = 1;
+  test_table->pages[page_idx].frame = 0x0;
+
+  kernel_directory.tables[table_idx] = test_table;
+  kernel_directory.physical_tables[table_idx] =
+      test_table_phys_addr | PG_PRESENT | PG_RW;
+
+  // lastly
+  kernel_directory.physical_address = (uint32_t)&kernel_directory;
 }
 
-void paging_load_directory(uint32_t dir) {
-  asm volatile("mov %0, %%cr3" ::"r"(dir));
+void paging_load_directory(page_directory_t* dir) {
+  asm volatile("mov %0, %%cr3" ::"r"(dir->physical_address));
 }
 
 static void paging_enable(void) {
@@ -48,7 +92,7 @@ static void paging_enable(void) {
 }
 
 void paging_finalize(void) {
-  paging_load_directory((uint32_t)page_dir);
+  paging_load_directory(&kernel_directory);
 
   // TODO: some more stuff
   isr_install_handler(14, paging_fault);
