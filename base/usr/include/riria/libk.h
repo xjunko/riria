554f4c0d89fc629c338a76c04ec6422d46f3a507
xjunko <phony@kafu.ovh>
2025-12-23 15:26:51 +0800

    libk: add some other functions limine might use

--- base/usr/include/riria/libk.h
+++ base/usr/include/riria/libk.h
@@ -1,12 +1,12 @@
 #pragma once
-#include <stdarg.h>
-#include <stddef.h>
-#include <stdint.h>
+#include <riria/types.h>
 
 void* malloc(size_t);
 
 void* memset(void*, int, size_t);
 void* memcpy(void* restrict, const void* restrict, size_t);
+void* memmove(void*, const void*, size_t);
+int memcmp(const void*, const void*, size_t);
 
 int vsprintf(char* buffer, const char* fmt, va_list args);
 void printk(const char* fmt, ...);
