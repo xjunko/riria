f318de778f3803d5130aa8269663db57ecc9226f
xjunko <phony@kafu.ovh>
2025-12-24 15:50:35 +0800

    types: declare ssize_t

--- base/usr/include/riria/types.h
+++ base/usr/include/riria/types.h
@@ -6,6 +6,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// we dont have a proper libc yet, so this will have to do for now
+#ifdef __i386__
+typedef uint32_t size_t;
+typedef int32_t ssize_t;
+#endif
+
+#ifdef __x86_64__
+typedef uint64_t size_t;
+typedef int64_t ssize_t;
+#endif
+
 #define UNUSED(x) (void)(x)
 
 void kprintf(const char*, ...);
