de76e168dc9976eacee226436fdbfd043e6e90d4
xjunko <phony@kafu.ovh>
2025-12-25 05:47:03 +0800

    cpu: improve gdt and tss

--- base/usr/include/riria/cpu/tss.h
+++ base/usr/include/riria/cpu/tss.h
@@ -7,17 +7,13 @@ typedef struct tss_entry {
   uint64_t rsp1;
   uint64_t rsp2;
   uint64_t reserved1;
-  uint64_t ist1;
-  uint64_t ist2;
-  uint64_t ist3;
-  uint64_t ist4;
-  uint64_t ist5;
-  uint64_t ist6;
-  uint64_t ist7;
+  uint64_t ist[7];
   uint64_t reserved2;
   uint16_t reserved3;
   uint16_t io_map_base;
 } __attribute__((packed)) tss_entry_t;
 
 void tss_set_stack(uintptr_t esp0);
+void write_tss(uint32_t num);
+
 extern void tss_flush(void);
--- kernel/arch/x86_64/gdt.c
+++ kernel/arch/x86_64/gdt.c
@@ -35,8 +35,6 @@ void gdt_set_gate(uint8_t idx, uint64_t base, uint64_t limit, uint8_t access,
   ENTRY(idx).access = access;
 }
 
-static void write_tss(uint32_t num, uint16_t ss0, uint32_t esp0);
-
 void gdt_install(void) {
   gdt_ptr_t* gdtp = &gdt.ptr;
   gdtp->limit = sizeof(gdt.entries) - 1;
@@ -47,41 +45,43 @@ void gdt_install(void) {
   gdt_set_gate(2, 0, 0, 0x92, 0x0);   // 0x10 - data segment
   gdt_set_gate(3, 0, 0, 0xF2, 0x0);   // 0x18 - user code segment
   gdt_set_gate(4, 0, 0, 0xFA, 0x20);  // 0x20 - user data segment
-  write_tss(5, 0x10, 0x0);            // 0x28 - TSS segment (1)
+  write_tss(5);                       // 0x28 - TSS segment (1)
 
   printf("[cpu] GDT=0x%x\n", gdtp->base);
   printf("[cpu] TSS=0x%x\n", (uintptr_t)&gdt.tss);
   printf("[cpu] GDT INIT...");
 
+  printf(" GDTP");
   gdt_flush((uintptr_t)gdtp);
+  printf(" TSS");
   tss_flush();
 
   printf(" OK!\n");  // it's not a proper osdev project if it doesnt have GDT
                      // INIT... OK /j
 }
 
-static void write_tss(uint32_t num, uint16_t ss0, uint32_t esp0) {
-  UNUSED(ss0);
-  UNUSED(esp0);
+void write_tss(uint32_t num) {
+  uint64_t base = (uintptr_t)&gdt.tss;
+  uintptr_t limit = sizeof(tss_entry_t) - 1;
 
-  tss_entry_t* tss = &gdt.tss;
-  uintptr_t base = (uintptr_t)tss;
-  uintptr_t limit = sizeof(*tss) - 1;
+  ENTRY(num).base_low = (base & 0xFFFF);
+  ENTRY(num).base_middle = (base >> 16) & 0xFF;
+  ENTRY(num).base_high = (base >> 24) & 0xFF;
 
-  gdt_set_gate(num, base, limit, 0x89, 0x00);
-  memset(tss, 0x0, sizeof(*tss));
+  ENTRY(num).limit_low = limit;
+  ENTRY(num).granularity = (limit >> 16) & 0x0F;
 
-  tss->ist1 = (uint64_t)(df_stack + sizeof(df_stack));
-  tss->ist2 = (uint64_t)(nmi_stack + sizeof(nmi_stack));
-  tss->rsp0 = (uint64_t)(kernel_stack + sizeof(kernel_stack));
+  ENTRY(num).access = 0x89;
 
   // HACK: bit cursed but it does the job for now
   uint32_t* hi = (uint32_t*)&gdt.entries[num + 1];
   hi[0] = (base >> 32);
   hi[1] = 0;
+
+  memset(&gdt.tss, 0, sizeof(tss_entry_t));
+  gdt.tss.ist[0] = (uint64_t)(df_stack + sizeof(df_stack));
+  gdt.tss.ist[1] = (uint64_t)(nmi_stack + sizeof(nmi_stack));
+  gdt.tss.rsp0 = (uint64_t)(kernel_stack + sizeof(kernel_stack));
 }
 
-void tss_set_stack(uintptr_t stack) {
-  gdt.tss.rsp0 = stack;
-  printf("[cpu] TSS.ESP0=0x%x\n", stack);
-}
+void tss_set_stack(uint64_t stack) { gdt.tss.rsp0 = stack; }
