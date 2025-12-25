811f4c76143ea5b84000d29bb24c7f42c6eba11f
xjunko <phony@kafu.ovh>
2025-12-26 03:55:26 +0800

    process: add state

--- base/usr/include/riria/process.h
+++ base/usr/include/riria/process.h
@@ -7,6 +7,13 @@
 #define USER_VIRT_START 0x10000
 #define USER_STACK_BASE 0x80000000
 
+typedef enum {
+  PROCESS_READY,
+  PROCESS_RUNNING,
+  PROCESS_SLEEPING,
+  PROCESS_DEAD,
+} process_state_t;
+
 typedef struct process {
   uint32_t id;
 
@@ -16,6 +23,7 @@ typedef struct process {
   uint64_t ursp;
   pagemap_t* pagemap;
 
+  process_state_t state;
   bool is_user;
 } process_t;
 
@@ -27,12 +35,15 @@ typedef struct process_node {
 typedef void (*process_entry_t)(void);
 
 void process_create(process_entry_t, pagemap_t*);
+void process_reap(void);
 int process_schedule(regs_t* r);
+void process_exit(int);
+
 process_t* process_get_current(void);
 
 __attribute__((noreturn)) void process_spawn_user(const uint8_t*, size_t,
                                                   uint64_t);
 __attribute__((noreturn)) void process_spawn_elf(uint8_t*, size_t);
 
-extern void process_switch(uint64_t*, uint64_t*);
+__attribute__((noreturn)) extern void process_switch(uint64_t*, uint64_t*);
 __attribute__((noreturn)) extern void switch_to_user(void);
