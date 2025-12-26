d7aff4a7645a31872227eb26a0c2ccea4f066dbe
xjunko <phony@kafu.ovh>
2025-12-27 01:05:59 +0800

    syscall: fork(2), read(3), open(5), close(6)
    fork is not properly implemented, it just panic when used.

--- kernel/system/syscall.c
+++ kernel/system/syscall.c
@@ -2,6 +2,7 @@
 #include <riria/cpu/msr.h>
 #include <riria/cpu/regs.h>
 #include <riria/drivers/serial.h>
+#include <riria/fs/vfs.h>
 #include <riria/process.h>
 #include <riria/syscall.h>
 #include <riria/types.h>
@@ -28,27 +29,92 @@ int syscall_exit(sysregs_t* r) {
   return 0;
 }
 
+#define SYSCALL_FORK 2
+int syscall_fork(sysregs_t* r) {
+  UNUSED(r);
+  UNREACHABLE();
+  return -1;
+}
+
+#define SYSCALL_READ 3
+int syscall_read(sysregs_t* r) {
+  int fd = r->rdi;
+  char* buf = (char*)r->rsi;
+  size_t len = r->rdx;
+
+  int ret = vfs_sys_read(fd, buf, len);
+  if (ret < 0) {
+    ret = -1;
+  }
+
+  return ret;
+}
+
 #define SYSCALL_WRITE 4
 int syscall_write(sysregs_t* r) {
+  // internally, we split the fd by two category, above the VFS_FD_OFFSET are
+  // normal fd, anything below is special case.
   int fd = r->rdi;
   char* buf = (char*)r->rsi;
   size_t len = r->rdx;
 
   printf("[sys] syscall_write: fd=%d buf=%p len=%d\n", fd, buf, len);
 
-  if (fd == 1) {
-    for (size_t i = 0; i < len; i++) {
-      char c = buf[i];
-      if (c == '\0') break;
-      serial_write(c);
-    }
+  // system fds
+  if (fd < VFS_FD_OFFSET) {
+    switch (fd) {
+      case 1: {
+        for (size_t i = 0; i < len; i++) {
+          char c = buf[i];
+          if (c == '\0') break;
+          kprintf("%c", c);
+        }
 
-    r->rax = len;
-    return len;
+        return len;
+      }
+      default:
+        return -1;
+    }
   }
 
-  r->rax = -1;
+  // user fds
+  if (fd >= VFS_FD_OFFSET) {
+    int ret = vfs_sys_write(fd, buf, len);
+    if (ret < 0) {
+      ret = -1;
+    }
+
+    return ret;
+  }
+
+  // not found
   return -1;
+}
+
+#define SYSCALL_OPEN 5
+int syscall_open(sysregs_t* r) {
+  char* path = (char*)r->rdi;
+  int flags = (int)r->rsi;
+  int mode = (int)r->rdx;
+
+  int ret = vfs_sys_open(path, flags, mode);
+  if (ret < 0) {
+    ret = -1;
+  }
+
+  return ret;
+}
+
+#define SYSCALL_CLOSE 6
+int syscall_close(sysregs_t* r) {
+  int fd = r->rdi;
+
+  int ret = vfs_sys_close(fd);
+  if (ret < 0) {
+    ret = -1;
+  }
+
+  return ret;
 }
 
 void syscall_handler(sysregs_t* r) {
@@ -61,8 +127,20 @@ void syscall_handler(sysregs_t* r) {
     case SYSCALL_EXIT:
       r->rax = syscall_exit(r);
       break;
+    case SYSCALL_FORK:
+      r->rax = syscall_fork(r);
+      break;
+    case SYSCALL_READ:
+      r->rax = syscall_read(r);
+      break;
     case SYSCALL_WRITE:
       r->rax = syscall_write(r);
+      break;
+    case SYSCALL_OPEN:
+      r->rax = syscall_open(r);
+      break;
+    case SYSCALL_CLOSE:
+      r->rax = syscall_close(r);
       break;
     default:
       r->rax = -1;
