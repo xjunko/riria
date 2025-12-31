0b602b6a74495fbeeed785abef2f4836b7051d62
xjunko <phony@kafu.ovh>
2025-12-31 09:33:44 +0800

    kernel: user process can be safely killed if we panicked

--- kernel/system/panic.c
+++ kernel/system/panic.c
@@ -1,9 +1,19 @@
 #include <riria/cpu/irq.h>
+#include <riria/process.h>
 #include <riria/types.h>
 #include <stdio.h>
 
 __attribute__((noreturn)) void panic(const char* message) {
   IRQ_OFF;
-  printf(ERROR "[ error] KERNEL PANIC: %s\n", message);
+  const bool is_user = (process_get_current()->type == PROCESS_USER);
+  const char* user_or_kernel = is_user ? "USER" : "KERNEL";
+
+  printf(ERROR "[ error] %s PANIC: %s\n", user_or_kernel, message);
+  if (is_user) {
+    printf(WARNING "[ error] Attempting to kill user process %u (name=%s)\n",
+           process_get_current()->id, process_get_current()->name);
+    process_exit(-1);  // hopefully thats all we needed to do.
+  }
+
   while (1) asm volatile("hlt");
 }
