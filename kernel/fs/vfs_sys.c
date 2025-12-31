f6b4bbd8aaab110ad65745d8e0285ead207f44cc
xjunko <phony@kafu.ovh>
2026-01-01 02:30:46 +0800

    vfs/syscall: add MLIBC_HACK
    sometimes i dont know when this occured, itll be nice to know

--- kernel/fs/vfs_sys.c
+++ kernel/fs/vfs_sys.c
@@ -1,6 +1,14 @@
 #include <riria/fs/vfs.h>
 #include <riria/types.h>
 
+static inline int MLIBC_HACK(int ret) {
+  if (ret < 0) {
+    printf(WARNING "[ vfs_sys] MLIBC_HACK occured: %d to 0\n", ret);
+    return 0;
+  }
+  return ret;
+}
+
 int vfs_sys_open(const char* path, int flags, int mode) {
   vfs_file_t* file = vfs_open(path, flags, mode);
   if (!file) return -1;
@@ -9,22 +17,17 @@ int vfs_sys_open(const char* path, int flags, int mode) {
 
 int vfs_sys_read(int fd, void* buffer, size_t sz) {
   vfs_file_t* file = vfs_get_from_fd(fd);
-  return vfs_read(file, buffer, sz);
+  return MLIBC_HACK(vfs_read(file, buffer, sz));
 }
 
 int vfs_sys_write(int fd, const void* buffer, size_t sz) {
   vfs_file_t* file = vfs_get_from_fd(fd);
-  return vfs_write(file, buffer, sz);
+  return MLIBC_HACK(vfs_write(file, buffer, sz));
 }
 
 int vfs_sys_seek(int fd, size_t offset, int whence) {
   vfs_file_t* file = vfs_get_from_fd(fd);
-  // HACK: mlibc straight up errors out if /stdout if the ret is negative
-  int ret = vfs_seek(file, offset, whence);
-  if (fd == 1) {
-    return ret < 0 ? 0 : ret;
-  }
-  return ret;
+  return MLIBC_HACK(vfs_seek(file, offset, whence));
 }
 
 void* vfs_sys_mmap(int fd, size_t* len, int prot, int flags) {
